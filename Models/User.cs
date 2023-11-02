5be6ce0a016bead82ef6951e23b6be16055a4d3e
emas20 <wierzbicki.przemek@gmail.com>
2023-11-02 13:58:59 +0100

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace Stomatologia.Models
+{
+    public class User
+    {
+            public int UserId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string PESEL { get; set; }
+            public string PhoneNumber { get; set; }
+        // Dodaj inne pola użytkownika, jeśli są wymagane
+
+        // Konstruktor
+        public User(string firstName, string lastName, string email, string password, string pesel, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PESEL = pesel;
+            PhoneNumber = phoneNumber;
+        }
+    }
+    }
+
