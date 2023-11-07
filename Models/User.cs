122dad1efefcb5affb865b3b54fab801351cd1cb
emas20 <wierzbicki.przemek@gmail.com>
2023-11-07 20:53:36 +0100

    edit-layout-and-usercontrolers

--- Models/User.cs
+++ Models/User.cs
@@ -1,18 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Stomatologia.Models
 {
-    public class User
+    public class User : IdentityUser
     {
       
-            [Key]
+           
+           // Dodaj niestandardowe właściwości użytkownika, jeśli są potrzebne
             public int UserId { get; set; }
+           // [Key]   
             public string FirstName { get; set; }
             public string LastName { get; set; }
-            public string Email { get; set; }
+            public override string Email { get; set; }
             public string Password { get; set; }
             public string PESEL { get; set; }
-            public string PhoneNumber { get; set; }
+            public override string  PhoneNumber { get; set; }
+            public DateTime DataUrodzenia { get; set; }
+
         // Dodaj inne pola użytkownika, jeśli są wymagane
         public User()
         {
