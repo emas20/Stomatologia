180e98e042d35e9a85a03cc841190bdf82818372
emas20 <wierzbicki.przemek@gmail.com>
2023-11-17 13:08:52 +0100

    create-editviewprofile

--- Data/Migrations/20231117120537_identityandprofileview.Designer.cs
+++ Data/Migrations/20231117120537_identityandprofileview.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stomatologia.Data;
 
@@ -11,9 +12,10 @@ using Stomatologia.Data;
 namespace Stomatologia.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231117120537_identityandprofileview")]
+    partial class identityandprofileview
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20231117120537_identityandprofileview.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stomatologia.Data.Migrations
+{
+    public partial class identityandprofileview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EditProfileViewModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Imie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nazwisko = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CurrentPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NewPassword = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ConfirmNewPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdentityUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EditProfileViewModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EditProfileViewModel_AspNetUsers_IdentityUserId",
+                        column: x => x.IdentityUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EditProfileViewModel_IdentityUserId",
+                table: "EditProfileViewModel",
+                column: "IdentityUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EditProfileViewModel");
+        }
+    }
+}
